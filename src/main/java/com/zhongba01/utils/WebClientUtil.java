785f6bfabe6823535457ed3df947df832677c1b9
Tang Jianhua <shmp2005@163.com>
2017-12-26 20:27:40 +0800

    fix bug

--- src/main/java/com/zhongba01/utils/WebClientUtil.java
+++ src/main/java/com/zhongba01/utils/WebClientUtil.java
@@ -1,8 +1,11 @@
 package com.zhongba01.utils;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.DefaultJavaScriptErrorListener;
 import com.zhongba01.VerifyCodeException;
 import org.apache.commons.lang3.RandomUtils;
 import org.jsoup.Jsoup;
@@ -12,6 +15,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
 
 /**
@@ -44,7 +49,7 @@ public final class WebClientUtil {
      */
     public static Document getDocument(String url, int seconds) {
         final HtmlPage htmlPage;
-        try (WebClient webClient = new WebClient(BrowserVersion.CHROME)) {
+        try (WebClient webClient = simpleWebClient()) {
             LOGGER.info("getDocument: " + url);
             Thread.sleep(seconds * 1000);
 
@@ -63,5 +68,28 @@ public final class WebClientUtil {
             e.printStackTrace();
             return string;
         }
+    }
+
+    private static WebClient simpleWebClient() {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.setCssErrorHandler(new SilentCssErrorHandler());
+        webClient.setIncorrectnessListener(new IncorrectnessListener() {
+            @Override
+            public void notify(String s, Object o) {
+                //do nothing
+            }
+        });
+        webClient.setJavaScriptErrorListener(new DefaultJavaScriptErrorListener() {
+            @Override
+            public void loadScriptError(HtmlPage page, URL scriptUrl, Exception exception) {
+                //do nothing
+            }
+
+            @Override
+            public void malformedScriptURL(HtmlPage page, String url, MalformedURLException malformedURLException) {
+                //do nothing
+            }
+        });
+        return webClient;
     }
 }
